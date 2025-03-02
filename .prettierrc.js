23407b3f35c279362592b2c843421e32b3c6f8eb
AbTheReactDev <abthereactdev@gmail.com>
2025-03-02 11:40:52 +0530

    Feat : Eslint and Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// .prettierrc.js
+module.exports = {
+    arrowParens: "always",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    jsxSingleQuote: false,
+    quoteProps: "as-needed",
+    printWidth: 120,
+    requirePragma: false,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: "es5",
+    useTabs: true,
+};
